91f5d0cd923aa79bd82424bfc6897bab43e1bd93
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-03-16 15:08:54 +0300

    convert facebook crate to use async Client

--- facebook/src/endpoints/accounts_endpoint.rs
+++ facebook/src/endpoints/accounts_endpoint.rs
@@ -9,16 +9,17 @@ pub struct AccountsEndpoint {
 }
 
 impl AccountsEndpoint {
-    pub fn list(&self) -> Result<Paging<Account>, FacebookError> {
+    pub async fn list(&self) -> Result<Paging<Account>, FacebookError> {
         let client = reqwest::Client::new();
 
         if let Some(access_token) = self.client.user_access_token.as_ref() {
-            let mut resp = client
+            let resp = client
                 .get(&format!("{}/me/accounts", &self.client.base_url))
                 .header("Authorization", format!("Bearer {}", &access_token))
-                .send()?;
+                .send()
+                .await?;
             //        let status = resp.status();
-            let value: serde_json::Value = resp.json()?;
+            let value: serde_json::Value = resp.json().await?;
             println!("{:?}", value.clone().to_string());
             let results: Paging<Account> = serde_json::from_value(value)?;
             Ok(results)
--- facebook/src/endpoints/official_events_endpoint.rs
+++ facebook/src/endpoints/official_events_endpoint.rs
@@ -2,9 +2,8 @@ use crate::error::{FacebookError, FacebookErrorResponse};
 use crate::facebook_client::FacebookClientInner;
 use crate::facebook_request::FacebookRequest;
 use crate::fbid::FBID;
-use log::Level::Info;
 use crate::nodes::Event;
-use reqwest::StatusCode;
+use log::Level::Info;
 use std::rc::Rc;
 
 pub struct OfficialEventsEndpoint {
@@ -12,7 +11,7 @@ pub struct OfficialEventsEndpoint {
 }
 
 impl OfficialEventsEndpoint {
-    pub fn create(&self, event: Event) -> Result<FBID, FacebookError> {
+    pub async fn create(&self, event: Event) -> Result<FBID, FacebookError> {
         if self.client.app_access_token.is_none() {
             return Err(FacebookError::Unauthorized);
         }
@@ -26,21 +25,24 @@ impl OfficialEventsEndpoint {
 
         jlog!(Info, "Sending request to Facebook", { "request": &request });
 
-        let mut resp = client
+        let resp = client
             .post(&format!("{}/v5.0/official_events", &self.client.base_url))
             .json(&request)
-            .send()?;
+            .send()
+            .await?;
 
-        let value: serde_json::Value = resp.json()?;
+        let status = resp.status();
+        let error_for_status = resp.error_for_status_ref().map(|_| ());
+        let value: serde_json::Value = resp.json().await?;
 
         jlog!(Info, "Response from Facebook", { "response": &value });
 
-        if resp.status() != StatusCode::OK {
+        if !status.is_success() {
             if let Some(error) = value.get("error") {
                 let error: FacebookErrorResponse = serde_json::from_value(error.clone())?;
                 return Err(FacebookError::FacebookError(error));
             }
-            resp.error_for_status()?;
+            error_for_status?;
         }
 
         #[derive(Deserialize)]
