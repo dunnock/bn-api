128a7c11a7e5d00e3b49ec95e5db403b9fb70449
Mike Berry <mikethetike@tari.com>
2020-03-30 15:45:43 +0200

    Fix compile errors

--- api/tests/functional/users.rs
+++ api/tests/functional/users.rs
@@ -511,6 +511,7 @@ async fn current_user() {
     assert_eq!(
         vec![
             Scopes::EventInterest,
+            Scopes::ListingWrite,
             Scopes::OrderReadOwn,
             Scopes::TransferCancelOwn,
             Scopes::TransferReadOwn,
@@ -563,6 +564,8 @@ async fn current_user_organization_owner() {
             "event:write",
             "hold:read",
             "hold:write",
+            "listing:write",
+            "loot-box:write",
             "note:delete",
             "note:read",
             "note:write",
@@ -578,6 +581,7 @@ async fn current_user_organization_owner() {
             "org:reports",
             "org:users",
             "org:write",
+            "rarity:write",
             "redeem:ticket",
             "scan-report:read",
             "settlement:read",
@@ -622,6 +626,7 @@ async fn current_user_organization_member() {
     assert_eq!(
         vec![
             Scopes::EventInterest,
+            Scopes::ListingWrite,
             Scopes::OrderReadOwn,
             Scopes::TransferCancelOwn,
             Scopes::TransferReadOwn,
